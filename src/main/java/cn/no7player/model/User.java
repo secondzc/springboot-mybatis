e6f3b7271082e7b14d43df93439dbc817960c6de
Unknown <1146923246@qq.com>
2018-03-15 08:34:22 +0800

    第一次提交，可以跑通

--- src/main/java/cn/no7player/model/User.java
+++ src/main/java/cn/no7player/model/User.java
@@ -4,9 +4,18 @@ package cn.no7player.model;
  * Created by zl on 2015/8/27.
  */
 public class User {
+    private Integer id;
     private String name;
     private Integer age;
     private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
